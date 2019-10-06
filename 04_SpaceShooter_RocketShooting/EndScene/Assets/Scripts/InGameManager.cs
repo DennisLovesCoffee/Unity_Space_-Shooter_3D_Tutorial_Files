941858ecbb708062938410c04860c7bd23bd2241
Dennis <vymerd@gmail.com>
2019-10-06 11:48:00 +0200

    Rocket Spawning

---
+++ 04_SpaceShooter_RocketShooting/EndScene/Assets/Scripts/InGameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameManager : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+    public Transform rocketSpawnPoint1, rocketSpawnPoint2;
+
+    public float fireInterval = 2f;
+    private bool canFire = true;
+
+    public void OnFireButtonClicked()
+    {
+        if (canFire)
+        {
+            //fire rocktes
+            FireRockets();
+
+            canFire = false;
+
+            StartCoroutine(ReloadDelay());
+        }
+    }
+
+    private void FireRockets()
+    {
+        Instantiate(rocketPrefab, rocketSpawnPoint1.position, Quaternion.identity);
+        Instantiate(rocketPrefab, rocketSpawnPoint2.position, Quaternion.identity);
+    }
+
+    private IEnumerator ReloadDelay()
+    {
+        //play reload sound 
+
+        yield return new WaitForSeconds(fireInterval);
+
+        canFire = true;
+    }
+
+    public void OnExitButtonClicked()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
---
+++ 04_SpaceShooter_RocketShooting/StartScene/Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    private Rigidbody rb;
+    private Vector3 randomRotation;
+    private float removePositionZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        randomRotation = new Vector3(Random.Range(0f, 100f), Random.Range(0f, 100f), Random.Range(0f, 100f));
+        removePositionZ = Camera.main.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z < removePositionZ)
+        {
+            Destroy(gameObject);
+        }
+
+        Vector3 movementVector = new Vector3(0f, 0f, -moveSpeed * Time.deltaTime);
+        rb.velocity = movementVector;
+
+        transform.Rotate(randomRotation * Time.deltaTime);
+    }
+
+    public void DestroyAsteroid()
+    {
+        //play particall effect
+
+        //disable movement
+
+        //disable colliders
+
+        //destroy game object with a delay
+        Destroy(gameObject);
+    }
+}
