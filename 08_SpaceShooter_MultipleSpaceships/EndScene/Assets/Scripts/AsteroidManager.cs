bed71c15c8eed3115963d9c3ea5ed65aca65a819
Dennis <vymerd@gmail.com>
2019-10-20 11:27:19 +0200

    Shop integration

---
+++ 08_SpaceShooter_MultipleSpaceships/EndScene/Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour
+{
+    #region Singleton
+    public static AsteroidManager Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    #endregion
+
+    public GameObject[] asteroidPrefabs;
+    public float asteroidSpawnDistance = 50f;
+
+    public float spawnTime = 2f;
+    private float timer = 0f;
+
+    [HideInInspector]
+    public float minX, maxX, minY, maxY;
+
+    public List<GameObject> aliveAsteroids = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = spawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= spawnTime)
+        {
+            //spawn asteroids
+            SpawnNewAsteroid();
+            timer = 0f;
+        }
+    }
+
+    private void SpawnNewAsteroid()
+    {
+        float newX = Random.Range(minX, maxX);
+        float newY = Random.Range(minX, maxY);
+
+        Vector3 spawPos = new Vector3(newX, newY, asteroidSpawnDistance);
+
+        GameObject GO = Instantiate(asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)], spawPos, Quaternion.identity);
+        aliveAsteroids.Add(GO);
+    }
+
+    public void UpdateAsteroids(List<GameObject> targetedAsteroids)
+    {
+        foreach(GameObject asterid in aliveAsteroids)
+        {
+            if (targetedAsteroids.Contains(asterid))
+            {
+                //red
+                asterid.GetComponent<AsteroidController>().SetTargetMaterial();
+            }
+            else
+            {
+                //reset it
+                asterid.GetComponent<AsteroidController>().ResetMaterial();
+            }
+        }
+    }
+
+
+}
---
+++ 08_SpaceShooter_MultipleSpaceships/StartScene/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] spaceshipPrefabs;
+    public Texture2D[] spaceshipTextures;
+
+    public static GameManager Instance;
+
+    public GameObject currentSpaceship;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+
+        spaceshipTextures = new Texture2D[spaceshipPrefabs.Length];
+        for(int i = 0; i < spaceshipPrefabs.Length; ++i)
+        {
+            GameObject prefab = spaceshipPrefabs[i];
+            Texture2D texture = AssetPreview.GetAssetPreview(prefab);
+            spaceshipTextures[i] = texture;
+        }
+
+        currentSpaceship = spaceshipPrefabs[0];
+
+    }
+
+    public void ChangeCurrentSpaceship(int idx)
+    {
+        currentSpaceship = spaceshipPrefabs[idx];
+    }
+}
